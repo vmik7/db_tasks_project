4c0d02f36fd18c0a2b31199544486a4dd7fd860c
vmik7 <vmik7@yandex.ru>
2021-05-26 14:12:21 +0300

    MySQL подключение

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require("express");
- 
+const mysql = require("mysql2");
+
 // Номер порта задаём константой
 const PORT = 80;
 const app = express();
- 
+
 // Подключаем шаблонизатор
 app.set('view engine', 'ejs');
 
+// Подключаемся к MySQL
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    database: "tasks_projects",
+    password: "root"
+});
+// connection.connect(function (err) {
+//     if (err) {
+//         return console.error("Ошибка: " + err.message);
+//     }
+//     else {
+//         console.log("Подключение к серверу MySQL успешно установлено");
+//     }
+// });
+
 // Обрабатываем корневой запрос
 app.get("/", (req, res) => {
-    res.render('index', {
-        msg: "Hello world! I'm using EJS :)"
+
+    let tasks;
+    connection.query("SELECT * FROM Задания", (err, results, fields) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            tasks = results;
+        }
+
+        res.render('index', {
+            msg: JSON.stringify(tasks)
+        });
     });
 });
 
